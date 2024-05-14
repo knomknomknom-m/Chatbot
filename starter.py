b869f870014f54735f2922c3be0a88ad44ab9707
knomknomknom-m <147120818+knomknomknom-m@users.noreply.github.com>
2024-05-14 16:26:44 +0900

    Update starter.py

--- starter.py
+++ starter.py
@@ -211,7 +211,7 @@ def apply_custom_css():
 apply_custom_css()
 
 # 보노보노 그림 추가
-st.image('D:/Users/mkw81/Desktop/4.jpeg', caption='Bonobono', width=200)
+st.image('D:/Users/mkw81/Desktop/4.jpeg', width='')
 
 
 # enter token
